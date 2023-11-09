d17ec783fd73d1fb2d02cf7b5646c7517b37a210
SudiptaPandaa <136814605+SudiptaPandaa@users.noreply.github.com>
2023-11-09 19:58:57 +0530

    Add files via upload

---
+++ pattern3.py
@@ -0,0 +1,20 @@
+import turtle
+import colorsys
+
+t=turtle.Turtle()
+s=turtle.Screen().bgcolor('black')
+t.color('white')
+t.speed(0)
+t.width(5)
+n = 200
+h = 0
+
+for i in range(180):
+    t.right(50)
+    for c in range(1):
+        t.forward(i*2)
+        c=colorsys.hsv_to_rgb(h,1,0.8)
+        h+=i/n
+        t.color(c)
+
+turtle.exitonclick()
